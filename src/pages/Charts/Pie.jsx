5b167fac1dc7105f7fe38497c59660f5ac933353
thefidanabdulla <thefidanabdulla@gmail.com>
2022-05-28 22:34:09 +0400

    Pie Chart is done

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import {AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, Inject, PieSeries} from '@syncfusion/ej2-react-charts';
+import { pieChartData } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
 
 const Pie = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Pie</div>
-  )
-}
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Pie" title="Project Cost Breakdown" />
+      <AccumulationChartComponent
+        id="chart-pie"
+        height="full"
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373E" : "#FFFFFF"}
+        legendSettings={{ background: "white" }}
+      >
+        <Inject services={[PieSeries]} />
+        <AccumulationSeriesCollectionDirective>
+          <AccumulationSeriesDirective 
+            dataSource={pieChartData}
+            name='Sale'
+            xName="x"
+            yName="y"
+            innerRadius="40%"
+            startAngle={0}
+            endAngle={360}
+            radius='70%'
+            explode
+            explodeOffset="10%"
+            explodeIndex={2}
+            dataLabel={{
+              visible: true,
+              name: 'text',
+              position: 'Inside',
+              font: {
+                fontWeight: '600',
+                color: '#fff'
+              }
+            }}
+          />
+        </AccumulationSeriesCollectionDirective>
+      </AccumulationChartComponent>
+    </div>
+  );
+};
 
-export default Pie
+export default Pie;
